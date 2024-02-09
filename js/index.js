8ea9ef10187a38a2a5465bba383c01bebd02e8c5
hgonzalezdvp <128415287+Hgonzalez91@users.noreply.github.com>
2024-02-09 16:57:58 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const btnSearch = document.getElementById('btnSearch');
+const getID = document.getElementById('getID')
+
+const API_Key = "at_eNZuDuEf9aVexX0iCDMoBsnUwcaNO";
+
+const geoIP = async (ip) => {
+    const getDataFromIPIFY = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${API_Key}&ipAddress=${ip}`)
+    const responseData = await getDataFromIPIFY.json();
+    console.log(responseData);
+    return responseData;
+}
+
+btnSearch.addEventListener('click', ()=>{
+    geoIP(getID.value);
+})
