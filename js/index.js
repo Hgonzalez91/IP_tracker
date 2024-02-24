de9ece56d62ef1d79e20e3d86252e98c8a9bb495
gonzaalzz <hectorlgonzalez91@gmail.com>
2024-02-24 18:10:45 -0400

    final

--- js/index.js
+++ js/index.js
@@ -1,18 +1,45 @@
 const btnSearch = document.getElementById('btnSearch');
-const getID = document.getElementById('getID')
+const getID = document.getElementById('getID');
 
 const API_Key = "at_eNZuDuEf9aVexX0iCDMoBsnUwcaNO";
 
-const geoIP = async (ip) => {
-    const getDataFromIPIFY = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${API_Key}&ipAddress=${ip}`)
-    const responseData = await getDataFromIPIFY.json();
-    console.log(responseData);
-    return responseData;
+let map;
+
+btnSearch.addEventListener('click', () => {
+    const geoIP = async (getID) => {
+        try {
+            const getDataFromIPIFY = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_Key}&ipAddress=${getID}`);
+            const responseData = await getDataFromIPIFY.json();
+            const lat = responseData.location.lat;
+            const lng = responseData.location.lng;
+            updateMap(lat, lng);
+            console.log(responseData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    geoIP(getID.value);
+});
+
+function updateMap(lat, lng) {
+    if (map) {
+        map.remove();
+    }
+    
+    map = L.map('myMap').setView([lat, lng], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup('You are here :)')
+        .openPopup();
+
+    map.on('click', onMapClick);
 }
 
-btnSearch.addEventListener('click', ()=>{
-    geoIP(getID.value)
-    .then((respIP) =>{
-
-    })
-});
+function onMapClick(e) {
+    alert(`Position: ${e.latlng}`);
+}
